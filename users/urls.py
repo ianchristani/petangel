28107c39c2edcbc098e9fefff44fe293a6582e2b
ianchristani <ianchristani@gmail.com>
2023-11-29 20:52:49 +0100

    polish version

--- users/urls.py
+++ users/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', views.index, name = 'index'),
     re_path(r'^signup/$', views.signup_view, name = "signup"),
     re_path(r'^login/$', views.login_view, name = "login"),
-    re_path(r'^logout/$', views.logout_view, name = "logout")
+    re_path(r'^logout/$', views.logout_view, name = "logout"),
 ]
