834063f2b340594f9eb91562cce927272bde7457
ianchristani <ianchristani@gmail.com>
2024-10-11 11:25:06 +0200

    preparing for the cleaning db routine

--- petangel/settings.py
+++ petangel/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = secretkey
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["zwierzeceanioly.com","127.0.0.1"]
 
 
 # Application definition
